824e392fa898f08c0719acd952d2f47a56180928
Papatopit <papatopit@gmail.com>
2022-10-26 19:25:27 +0300

    init db t4

---
+++ db/src/main/java/com/socialNet/netDb/SocialNetDatabaseApp.java
@@ -0,0 +1,11 @@
+package com.socialNet.netDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetDatabaseApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetDatabaseApp.class,args);
+    }
+}
