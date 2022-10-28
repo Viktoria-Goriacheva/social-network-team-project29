cb82b42a5a32abc757460c412d625244b377ad1a
Oleg Pohilko <pohilkoo3@rambler.ru>
2022-10-28 17:05:39 +0300

    Add front's port in application.yml

--- core/src/main/java/ru/socialnet/team29/filters/SimpleCorsFilter.java
+++ core/src/main/java/ru/socialnet/team29/filters/SimpleCorsFilter.java
@@ -1,5 +1,6 @@
 package ru.socialnet.team29.filters;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -9,6 +10,9 @@ import java.io.IOException;
 @Component
 public class SimpleCorsFilter implements Filter {
 
+    @Value("${frontHost}")
+    private String frontHost;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -17,7 +21,7 @@ public class SimpleCorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:8098");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", frontHost);
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
