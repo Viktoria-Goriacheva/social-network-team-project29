3da0b73f7f3ef62e0603c9381cf6c2f7720a7964
Oleg Pohilko <pohilkoo3@rambler.ru>
2022-12-12 14:47:54 +0300

    Отменил все изменения которые были внесены при добавлении DslContext

--- db/src/main/java/ru/socialnet/team29/repository/PostFileRepository.java
+++ db/src/main/java/ru/socialnet/team29/repository/PostFileRepository.java
@@ -5,18 +5,26 @@ import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 import ru.socialnet.team29.domain.tables.PostFile;
 import ru.socialnet.team29.domain.tables.records.PostFileRecord;
+import ru.socialnet.team29.services.DslContextCustom;
 
 @Repository
 @RequiredArgsConstructor
 public class PostFileRepository {
 
-  private final DSLContext dsl;
+    private final DslContextCustom dslContextCustom;
+    private static DSLContext dsl;
 
-  public PostFileRecord findPathByIdPost(Integer id) {
+    public PostFileRecord findPathByIdPost(Integer id) {
+        initDsl();
+        return dsl.selectFrom(PostFile.POST_FILE)
+                .where(PostFile.POST_FILE.POST_ID.eq(id))
+                .fetchAny();
+    }
 
-    return dsl.selectFrom(PostFile.POST_FILE)
-        .where(PostFile.POST_FILE.POST_ID.eq(id))
-        .fetchAny();
-  }
+    private void initDsl() {
+        if (dsl == null) {
+            dsl = dslContextCustom.initDslContext();
+        }
+    }
 
 }
--- db/src/main/java/ru/socialnet/team29/repository/PostRepository.java
+++ db/src/main/java/ru/socialnet/team29/repository/PostRepository.java
@@ -6,12 +6,14 @@ import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 import ru.socialnet.team29.domain.tables.PostTable;
 import ru.socialnet.team29.domain.tables.records.PostTableRecord;
+import ru.socialnet.team29.services.DslContextCustom;
 
 @Repository
 @RequiredArgsConstructor
 public class PostRepository {
 
-  private final DSLContext dsl;
+  private final DslContextCustom dslContextCustom;
+  private static DSLContext dsl;
 
 
   public PostTableRecord insert(PostTableRecord post) {
@@ -24,6 +26,7 @@ public class PostRepository {
 
 
   public List<PostTableRecord> findAllById(Integer id) {
+    initDsl();
     return dsl.selectFrom(PostTable.POST_TABLE)
         .where(PostTable.POST_TABLE.AUTHOR_ID.eq(id))
         .and(PostTable.POST_TABLE.IS_BLOCKED.eq(false))
@@ -34,9 +37,15 @@ public class PostRepository {
 
 
   public Boolean delete(Integer id) {
+    initDsl();
     return dsl.deleteFrom(PostTable.POST_TABLE)
         .where(PostTable.POST_TABLE.ID.eq(id))
         .execute() == 1;
+  }
+  private void initDsl() {
+    if (dsl == null) {
+      dsl = dslContextCustom.initDslContext();
+    }
   }
 
 }
